d1c6a26e296f3a89920a6b2f884d98dc2b7187e4
Usama Sheikh <osamasheikh8844@gmail.com>
2024-05-14 10:40:01 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,38 +8,16 @@ use Illuminate\Support\Facades\Route;
 | Admin Portal Route
 |--------------------------------------------------------------------------
 */
-require __DIR__ . '/admin.php';
-/*------------------------------------------------------------------------*/
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AuthenticationController::class, 'login'])->name('admin.authentication.login');
+    Route::get('/forget/password', [AuthenticationController::class, 'forgot'])->name('admin.authentication.forgot');
+    Route::get('/password/success/{email}/{password}', [AuthenticationController::class, 'resetPasswordSuccess'])->name('admin.authentication.resetPassword.success');
+    Route::get('/magically/login/{email}/{password}', [AuthenticationController::class, 'magicallyLogin'])->name('admin.authentication.magically.login');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Neurologist Portal Route
-|--------------------------------------------------------------------------
-*/
-require __DIR__ . '/neurologist.php';
-/*------------------------------------------------------------------------*/
+Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
+    Route::get('/', [App\Http\Controllers\Admin\Dashboard\DashboardController::class, 'dashboard'])->name('admin.dashboard');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Practitioner Portal Route
-|--------------------------------------------------------------------------
-*/
-require __DIR__ . '/practitioner.php';
-/*------------------------------------------------------------------------*/
-
-/*
-|--------------------------------------------------------------------------
-| Check User and Redirect to Relevant Portal Route
-|--------------------------------------------------------------------------
-*/
 Route::get('/', function () {})->middleware('check.user');
-/*------------------------------------------------------------------------*/
-
-/*
-|--------------------------------------------------------------------------
-| Default Authentication Route
-|--------------------------------------------------------------------------
-*/
 require __DIR__ . '/auth.php';
-/*------------------------------------------------------------------------*/
-
