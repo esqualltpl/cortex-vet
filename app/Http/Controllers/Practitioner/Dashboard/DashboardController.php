07bdcf65a7f845dd9dffb2e977e5e53a8fd03d1c
Usama Sheikh <osamasheikh8844@gmail.com>
2024-06-25 11:01:41 +0500

    After successfully creating the patient system should direct user to dashboard main screen

--- app/Http/Controllers/Practitioner/Dashboard/DashboardController.php
+++ app/Http/Controllers/Practitioner/Dashboard/DashboardController.php
@@ -127,7 +127,7 @@ class DashboardController extends Controller
             $response = ResponseMessage::ResponseNotifySuccess('Success!', 'New patient added successfully.');
             Log::info('Successfully add the new patient', ['updated' => 'success' ?? '']);
             DB::commit();
-           return redirect()->back()->with($response);
+           return redirect()->route('practitioner.dashboard')->with($response);
         } catch (Exception $e) {
             DB::rollBack();
             $response = ResponseMessage::ResponseNotifyError('Error!', 'The system is unable to add the new patient. Please try again later.');
