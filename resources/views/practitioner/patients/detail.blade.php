0f2dfb42513acaf0db7ff6abfcc717418f0b9377
Usama Sheikh <osamasheikh8844@gmail.com>
2024-08-01 14:39:33 +0500

    Functional Fixes
    Show text on hovering as "Mail Report" and "Edit" also change 2nd icon to edit...and make both icons colored. Edit icon should direct user to the last step of neuro assessment

--- resources/views/practitioner/patients/detail.blade.php
+++ resources/views/practitioner/patients/detail.blade.php
@@ -199,11 +199,11 @@
                                    class="text-info text-decoration-underline"> Neuro Exam {{ $sn }}</a>
                             </td>
                             <td>
-                                <span class="cursor-pointer show-export-modal" data-neuro-assessment-id="{{ Crypt::encrypt($appointmentHistory->id) }}">
-                                    <img src="{{ asset('portal/assets/img/Email gray.png') }}" style=" width: 22px !important; margin-left: 2px !important; " alt="icon">
+                                <span data-toggle="tooltip" data-placement="top" title="Mail Report" class="cursor-pointer show-export-modal" data-neuro-assessment-id="{{ Crypt::encrypt($appointmentHistory->id) }}">
+                                    <img src="{{ asset('portal/assets/img/Email.png') }}" style=" width: 22px !important; margin-left: 2px !important; " alt="icon">
                                 </span>
-                                <a href="{{ route('practitioner.neuro.assessment.detail', ['id'=>Crypt::encrypt($appointmentHistory->patientInfo?->id), 'na_id'=>Crypt::encrypt($appointmentHistory->id)]) }}">
-                                    <img src="{{ asset('portal/assets/img/Veterinary Practitioners gray.png') }}" style=" width: 20px !important; margin-left: 2px !important; " alt="icon">
+                                <a data-toggle="tooltip" data-placement="top" title="Edit" href="{{ route('practitioner.neuro.assessment.detail', ['id'=>Crypt::encrypt($appointmentHistory->patientInfo?->id), 'na_id'=>Crypt::encrypt($appointmentHistory->id)]) }}">
+                                    <img src="{{ asset('portal/assets/img/edit png.png') }}" style=" width: 20px !important; margin-left: 2px !important; " alt="icon">
                                 </a>
                             </td>
                         </tr>
